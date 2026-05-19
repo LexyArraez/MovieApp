76107b42ec90950e048211a44f1d0bed9a3effd9
Your Name <lexy.arraez.1909@icloud.com>
2026-05-19 09:47:21 +0200

    chore/ crear

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { MovieLogo } from "./components/landing/MovieLogo"
+import './index.css'
+
+function App() {
+    return (
+        <div>
+           
+        </div>
+    )
+}
+
+export default App
